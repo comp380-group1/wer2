3176de5abc855b0e8b689684f293a9a360a5bc18
MattHamersky <hamerskymatthew@yahoo.com>
2013-12-20 15:22:33 -0800

    Fixed the "texting" bug where the person who was supposed to receive money, got the text instead of the person who was supposed to pay it.

--- src/main/Reconciler.java
+++ src/main/Reconciler.java
@@ -19,7 +19,7 @@ public class Reconciler {
 					
 					//output.add((new Payment(String.valueOf(input.get(j).getId()), String.valueOf(input.get(i).getId()), input.get(j).getCurrentBalance())));
 					output.add((new Payment(input.get(j).getName(), input.get(i).getName(), input.get(j).getCurrentBalance(), input.get(j).getPhoneNumber())));
-					Log.i("group1", "hi");
+					Log.i("group1 - First Constructor", input.get(j).getName() + " - " + input.get(i).getName() + " - " + input.get(j).getPhoneNumber());
 					input.get(i).setBalance(0);
 					input.get(j).setBalance(0);
 					break;
@@ -41,6 +41,7 @@ public class Reconciler {
 					
 					//output.add(new Payment(String.valueOf(input.get(j).getId()), String.valueOf(input.get(i).getId()), input.get(j).getCurrentBalance()));
 					output.add(new Payment(input.get(j).getName(), input.get(i).getName(), input.get(j).getCurrentBalance(), input.get(j).getPhoneNumber()));
+					Log.i("group1 - Second Constructor", input.get(j).getName() + " - " + input.get(i).getName() + " - " + input.get(j).getPhoneNumber());
 					input.get(i).setBalance(result);
 					input.get(j).setBalance(0);
 					simpleReconcile(input, output);
@@ -50,7 +51,8 @@ public class Reconciler {
 				else if (result > 0 && input.get(i).getCurrentBalance() != 0 && input.get(j).getCurrentBalance() != 0) {
 					
 					//output.add(new Payment(String.valueOf(input.get(j).getId()), String.valueOf(input.get(i).getId()), Math.abs(input.get(i).getCurrentBalance())));
-					output.add(new Payment(input.get(j).getName(), input.get(i).getName(), Math.abs(input.get(i).getCurrentBalance()), input.get(i).getPhoneNumber()));
+					output.add(new Payment(input.get(j).getName(), input.get(i).getName(), Math.abs(input.get(i).getCurrentBalance()), input.get(j).getPhoneNumber()));
+					Log.i("group1 - Third Constructor", input.get(j).getName() + " - " + input.get(i).getName() + " - " + input.get(j).getPhoneNumber());
 					input.get(i).setBalance(0);
 					input.get(j).setBalance(result);
 					simpleReconcile(input, output);
